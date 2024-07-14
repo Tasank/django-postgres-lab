39066a06eef830f54cc1c721d6f92e63b27efa67
Tasank <apanasovich04@mail.ru>
2024-07-14 11:55:58 +0700

    локализация

--- Django_Beginning/game/views.py
+++ Django_Beginning/game/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.utils.translation import gettext as _ # импортируем функцию для перевода
 from django.views import View
 from django.http import HttpResponse
+from .models import MyModel
 
 
 class Index(View):
     def get(self, request):
-        string = _('Hello world')
+        models = MyModel.objects.all()
 
-        return HttpResponse(string)
-
+        context = {
+            'models': models,
+        }
+        return HttpResponse(render(request, 'index.html', context))
 def index(request):
     return render(request, 'index.html')
